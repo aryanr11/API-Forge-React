3d229a92847f1f2e8bc9e36b391e87677b49db43
Aryan rathod <aryancreative99@gmail.com>
2024-04-02 08:58:09 +0530

    .

--- src/componet/Codebox.js
+++ src/componet/Codebox.js
@@ -48,19 +48,6 @@ const Codbox = (props) => {
                   className="circle"
                 ></Item>
               </Stack>
-              <Item sx={{ color: "#FFFFFF", paddingLeft: "10px" }}>
-                <Button
-                  sx={{ backgroundColor: "transparent", marginLeft: "5px" }}
-                >
-                  <ContentCopyIcon
-                    sx={{
-                      margin: "0px 10px",
-                      verticalAlign: "center",
-                      fontSize: { xs: "20px" },
-                    }}
-                  />
-                </Button>
-              </Item>
             </Stack>
           </Grid>
           <Grid
